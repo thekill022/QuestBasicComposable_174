2e9c0f60a2c0db58e31dbc19127120125fc0f630
thekill022 <gibfathoni@gmail.com>
2025-10-17 10:27:31 +0700

    Membuat function untuk UI tataletak Column

--- app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
@@ -1,2 +1,20 @@
 package com.example.myapplication2.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TataletakColumn(modifier : Modifier) {
+    Column(
+        modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3" )
+        Text(text = "Komponen4")
+    }
+}
