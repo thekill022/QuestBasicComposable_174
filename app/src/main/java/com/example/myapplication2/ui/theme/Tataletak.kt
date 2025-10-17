b3fd8182a8d030a3237a744498f0ff69d041573b
thekill022 <gibfathoni@gmail.com>
2025-10-17 10:32:48 +0700

    Membuat function untuk UI tataletak Box

--- app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
@@ -1,12 +1,15 @@
 package com.example.myapplication2.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -32,5 +35,21 @@ fun TataletakRow(modifier: Modifier) {
         Text(text = "Komponen2")
         Text(text = "Komponen3" )
         Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1" )
+        Text(text = "Box 2")
+        Text(text = "Column 2")
     }
 }
