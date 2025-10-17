4733522e47ff98310b5cc25ac4cfd95e443d6b4e
thekill022 <gibfathoni@gmail.com>
2025-10-17 10:29:49 +0700

    Membuat function untuk UI tataletak Row

--- app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication2.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +14,19 @@ import androidx.compose.ui.unit.dp
 fun TataletakColumn(modifier : Modifier) {
     Column(
         modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3" )
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
