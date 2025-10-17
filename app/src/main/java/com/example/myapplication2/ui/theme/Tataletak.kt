51664fdfc12e16765c76f2afd384279b44f71ab8
thekill022 <gibfathoni@gmail.com>
2025-10-17 10:54:04 +0700

    menampilkan image

--- app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication2.ui.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,8 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication2.R
 
 @Composable
@@ -123,5 +128,11 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(modifier = modifier
+            .fillMaxWidth()
+            .height(height = 300.dp)
+            .background(color = Color.Cyan)) {
+            Image(painter = gambar, contentDescription = null, contentScale = ContentScale.Fit )
+        }
     }
 }
