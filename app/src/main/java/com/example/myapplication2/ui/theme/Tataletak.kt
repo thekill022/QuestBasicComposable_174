b38c5fa6fd31f1012159caca04541fdfcd48af4b
thekill022 <gibfathoni@gmail.com>
2025-10-17 10:48:40 +0700

    membuat tataletak box yang memiliki column lalu didalam column ada beberapa row

--- app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
+++ app/src/main/java/com/example/myapplication2/ui/theme/Tataletak.kt
@@ -1,17 +1,23 @@
 package com.example.myapplication2.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.myapplication2.R
 
 @Composable
 fun TataletakColumn(modifier : Modifier) {
@@ -83,6 +89,38 @@ fun TataletakRowColumn(modifier: Modifier) {
             Text(text = "Komponen1Kolom1")
             Text(text = "Komponen2Kolom1")
             Text(text = "Komponen3Kolom1")
+        }
+    }
+}
+
+@Composable
+fun TataletakBoxColumnRow(modifier: Modifier) {
+    val gambar = painterResource(id = R.drawable.cat)
+    Column {
+        Box(modifier = modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center) {
+
+            Column {
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
+            }
         }
     }
 }
